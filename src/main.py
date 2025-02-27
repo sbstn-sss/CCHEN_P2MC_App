898d09c52f365d446eb349d353e35ebe79e58efe
sbstn-sss <sebastian.osses@sansano.usm.cl>
2025-02-27 15:16:17 -0300

    agregando interfaz preliminar y main.py

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5 import QtWidgets, QtCore
+
+from views.ui_mainwindow import Ui_MainWindow
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
+
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
